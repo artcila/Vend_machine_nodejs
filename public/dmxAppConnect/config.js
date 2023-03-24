2808a71475a0106d1ff11d2b0eea140827a3ccfb
asada <hearteater87@gmail.com>
2023-03-24 15:13:26 +0100

    first

---
+++ public/dmxAppConnect/config.js
@@ -0,0 +1,29 @@
+dmx.config({
+  "index": {
+    "masonry1": {
+      "meta": [
+        {
+          "type": "text",
+          "name": "id"
+        },
+        {
+          "type": "text",
+          "name": "name"
+        },
+        {
+          "type": "text",
+          "name": "image"
+        },
+        {
+          "type": "text",
+          "name": "price"
+        },
+        {
+          "type": "text",
+          "name": "stock"
+        }
+      ],
+      "outputType": "array"
+    }
+  }
+});
